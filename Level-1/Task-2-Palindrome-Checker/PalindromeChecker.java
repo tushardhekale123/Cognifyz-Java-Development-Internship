7d2c3774f2311e59c2866768b846ae676ec474be
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-17 22:56:35 +0530

    complete

--- Level-1/Task-2-Palindrome-Checker/PalindromeChecker.java
+++ Level-1/Task-2-Palindrome-Checker/PalindromeChecker.java
@@ -1,29 +1,56 @@
+// import java.util.Scanner;
+
+// public class PalindromeChecker {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("===== Palindrome Checker =====");
+
+//         System.out.print("Enter a word: ");
+//         String word = sc.nextLine();
+
+//         String original = word.toLowerCase();
+//         String reverse = "";
+
+//         for (int i = original.length() - 1; i >= 0; i--) {
+//             reverse = reverse + original.charAt(i);
+//         }
+
+//         if (original.equals(reverse)) {
+//             System.out.println(word + " is a Palindrome.");
+//         } else {
+//             System.out.println(word + " is NOT a Palindrome.");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+import java.util.Random;
 import java.util.Scanner;
 
-public class PalindromeChecker {
-
+class PalindromeChecker {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int secret = 70;
 
-        System.out.println("===== Palindrome Checker =====");
+        while (true) { 
+        System.out.println("guess the no");
+        int guess= sc.nextInt();z
 
-        System.out.print("Enter a word: ");
-        String word = sc.nextLine();
-
-        String original = word.toLowerCase();
-        String reverse = "";
-
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reverse = reverse + original.charAt(i);
+        if (secret == guess) {
+            System.out.println("Congratulations! You guessed the correct number.");
+            break;
         }
-
-        if (original.equals(reverse)) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
+        else if (secret >= guess) {
+            System.out.println("Too Low! Try Again.");
         }
-
-        sc.close();
+        else if (secret <= guess) {
+            System.out.println("Too High! Try Again.");
+           }
+        }
     }
 }
